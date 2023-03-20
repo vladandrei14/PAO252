f8d8a7bf0b51ce7f52fe81e535bef3e0b5319010
Vlad Balauta <Vlad.Balauta@endava.com>
2023-03-20 14:14:46 +0200

    Added courses

--- lab04/src/Main.java
+++ lab04/src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Animal a = new Mamifer();
+        Animal animal = new Mamifer();
 
 
-        // Compilation error because a is a Animal type reference and Animal does not have an <canisa> atribute
+        // Compilation error because animal is a Animal type reference and Animal does not have an <canisa> atribute
         // check references.png
         System.out.println(a.canisa);
     }
